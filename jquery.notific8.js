b54ab6508cfe4130d45394ac045781955c42bddf
Will Steinmetz <willsteinmetz@me.com>
2014-03-19 14:11:30 -0400

    Grunt tasks ready closes #18

--- jquery.notific8.js
+++ jquery.notific8.js
@@ -1,8 +1,6 @@
 /**
  * @author Will Steinmetz
- * 
  * jQuery notification plug-in inspired by the notification style of Windows 8
- * 
  * Copyright (c)2013, Will Steinmetz
  * Licensed under the BSD license.
  * http://opensource.org/licenses/BSD-3-Clause
