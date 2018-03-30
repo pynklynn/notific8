7e6c98f07fbcaa9ca1d16485244b3c79c82b5f2c
Will Steinmetz <willsteinmetz@me.com>
2018-03-29 20:47:58 -0400

    Adding the tsconfig and tslint. Created the options interface.

---
+++ src/notific8/notific8-options.d.ts
@@ -0,0 +1,12 @@
+export interface Notific8Options {
+  closeText: string;
+  horizontalEdge: 'top'|'bottom';
+  life: number;
+  queue: boolean;
+  sticky: boolean;
+  styleNamespace: string;
+  theme: string;
+  themeColor: string;
+  verticalEdge: 'left'|'right';
+  zIndex: number;
+}
