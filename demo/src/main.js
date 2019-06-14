9bb1d4d0cd3b3db0b2a1aff0df24ea6f17112f95
Will Steinmetz <willsteinmetz@me.com>
2019-06-14 18:41:59 -0400

    Updating package versions, license, and copyright

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright (c) 2013-2019 Pynk Lynn, LLC
+ * This code may only be used under the MIT style license found at
+ * https://github.com/pynklynn/notific8/blob/master/LICENSE
+ */
+
 import Vue from 'vue';
 import App from './App.vue';
 
